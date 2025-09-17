e4426f5e7f6568e9fbf241b536a1aa33069dceb0
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-09-17 13:56:43 -0300

    Update Most_played_artists.sql

--- MySQL/Queries/Most_played_artists.sql
+++ MySQL/Queries/Most_played_artists.sql
@@ -1,6 +1,5 @@
 SELECT `artist_name` AS 'Artist name', 
 COUNT(`artist_name`) AS 'Times the artist was played',
-ROUND(SUM(`time_played`) / 60000, 2) AS 'Total minutes listened',
 ROUND(SUM(`time_played`) / 3600000, 2) AS 'Total hours listened'
 FROM `streaming_info`
 WHERE YEAR(`date`) = '2025'
