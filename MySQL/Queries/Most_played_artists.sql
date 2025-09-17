b20c8bf4d8ddcb84b8b75c1112fe5fec49c1f05b
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-09-17 10:13:12 -0300

    Add files via upload

---
+++ MySQL/Queries/Most_played_artists.sql
@@ -0,0 +1,9 @@
+SELECT `artist_name` AS 'Artist name', 
+COUNT(`artist_name`) AS 'Times the artist was played',
+ROUND(SUM(`time_played`) / 60000, 2) AS 'Total minutes listened',
+ROUND(SUM(`time_played`) / 3600000, 2) AS 'Total hours listened'
+FROM `streaming_info`
+WHERE YEAR(`date`) = '2025'
+GROUP BY `artist_name`
+ORDER BY SUM(`time_played`) / 60000 DESC
+LIMIT 10;
